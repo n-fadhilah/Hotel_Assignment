4ec12a40b77a06487393d4e34f7e70706267f5e6
Fadhilah <nurfadhilah.ahamid@gmail.com>
2023-07-04 12:25:41 +0800

    feature: added missing items

---
+++ app/config/api.config.js
@@ -0,0 +1,5 @@
+const apiUrls = [
+  "https://5f2be0b4ffc88500167b85a0.mockapi.io/suppliers/paperflies",
+  "https://5f2be0b4ffc88500167b85a0.mockapi.io/suppliers/patagonia",
+  "https://5f2be0b4ffc88500167b85a0.mockapi.io/suppliers/acme",
+];
