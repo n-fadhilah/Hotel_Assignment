fa59cf7d9033839427a2c0c0c7516937a0ed330c
Fadhilah <nurfadhilah.ahamid@gmail.com>
2023-07-05 21:45:20 +0800

    refactor: refactor codes on controller

--- app/controllers/hotel.controller.js
+++ app/controllers/hotel.controller.js
@@ -7,16 +7,20 @@ exports.getallhotels = async (req, res, client) => {
     const allHotels = [];
     const apiRequests = apiUrls.map((url) => axios.get(url));
     const responses = await Promise.all(apiRequests);
+
     const allhotelCollection = client.db().collection("allHotels");
 
-    responses.forEach(async (response, index) => {
+    for (let index = 0; index < responses.length; index++) {
+      const response = responses[index];
       allHotels.push(...response.data);
       const supplierName = `supplier_${index + 1}`;
-      saveHotelstoDb(response, supplierName, allhotelCollection);
-      console.log(`Inserted/updated hotel data in collection ${supplierName}`);
-    });
+      const supplierCollection = client.db().collection(supplierName);
 
-    res.json(allHotels);
+      await saveHotelstoDb(response, supplierCollection, allhotelCollection);
+    }
+
+    const hotelsData = await allhotelCollection.find().toArray();
+    res.json(hotelsData);
   } catch (error) {
     console.error("An error occurred while fetching hotel data:", error);
     res.status(500).json({ error: "Failed to fetch hotel data" });
@@ -24,21 +28,43 @@ exports.getallhotels = async (req, res, client) => {
   }
 };
 
-function saveHotelstoDb(response, supplierName, allhotelCollection) {
-  const supplierCollection = client.db().collection(supplierName);
+async function saveHotelstoDb(
+  response,
+  supplierCollection,
+  allhotelCollection
+) {
+  const updatePromises = response.data.map(async (document) => {
+    const {
+      hotel_id: hotelId,
+      Id,
+      id,
+      DestinationId,
+      destination_id,
+      destination,
+      ...cleanDocument
+    } = document;
 
-  response.data.forEach(async (document) => {
     const query = {
-      hotelId: document.hotel_id ?? document.Id ?? document.id,
-      destinationId:
-        document.DestinationId ??
-        document.destination_id ??
-        document.destination,
+      hotelId: hotelId || Id || id,
+      destinationId: DestinationId || destination_id || destination,
     };
-    const update = { $set: document };
+    const update = { $set: cleanDocument };
     const options = { upsert: true };
     delete document._id;
-    await supplierCollection.updateOne(query, update, options);
-    await allhotelCollection.updateOne(query, update, options);
+
+    const supplierUpdatePromise = supplierCollection.updateOne(
+      query,
+      update,
+      options
+    );
+    const allHotelUpdatePromise = allhotelCollection.updateOne(
+      query,
+      update,
+      options
+    );
+
+    return Promise.all([supplierUpdatePromise, allHotelUpdatePromise]);
   });
+
+  await Promise.all(updatePromises);
 }
