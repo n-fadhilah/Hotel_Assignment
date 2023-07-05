30f65605f9e9c2d69418a55a62fc8cef279a2acc
Fadhilah <nurfadhilah.ahamid@gmail.com>
2023-07-05 19:52:31 +0800

    refactor: extracted bulk of code to function

--- app/controllers/hotel.controller.js
+++ app/controllers/hotel.controller.js
@@ -8,26 +8,11 @@ exports.getallhotels = async (req, res, client) => {
     const apiRequests = apiUrls.map((url) => axios.get(url));
     const responses = await Promise.all(apiRequests);
     const allhotelCollection = client.db().collection("allHotels");
+
     responses.forEach(async (response, index) => {
       allHotels.push(...response.data);
       const supplierName = `supplier_${index + 1}`;
-      const supplierCollection = client.db().collection(supplierName);
-
-      response.data.forEach(async (document) => {
-        const query = {
-          hotelId: document.hotel_id ?? document.Id ?? document.id,
-          destinationId:
-            document.DestinationId ??
-            document.destination_id ??
-            document.destination,
-        };
-        const update = { $set: document };
-        const options = { upsert: true };
-        delete document._id;
-        await supplierCollection.updateOne(query, update, options);
-        await allhotelCollection.updateOne(query, update, options);
-      });
-
+      saveHotelstoDb(response, supplierName, allhotelCollection);
       console.log(`Inserted/updated hotel data in collection ${supplierName}`);
     });
 
@@ -38,3 +23,22 @@ exports.getallhotels = async (req, res, client) => {
     return error;
   }
 };
+
+function saveHotelstoDb(response, supplierName, allhotelCollection) {
+  const supplierCollection = client.db().collection(supplierName);
+
+  response.data.forEach(async (document) => {
+    const query = {
+      hotelId: document.hotel_id ?? document.Id ?? document.id,
+      destinationId:
+        document.DestinationId ??
+        document.destination_id ??
+        document.destination,
+    };
+    const update = { $set: document };
+    const options = { upsert: true };
+    delete document._id;
+    await supplierCollection.updateOne(query, update, options);
+    await allhotelCollection.updateOne(query, update, options);
+  });
+}
